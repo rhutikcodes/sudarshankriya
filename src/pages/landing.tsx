938c1f333c76f0cf29c72fea462d98da4ed00685
Rhutik <rhutikgiradkar@gmail.com>
2023-08-20 00:23:12 +0530

    finish

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -3,14 +3,11 @@ import DownArrow from "@/Svg/DownArrow";
 import UpArrow from "@/Svg/UpArrow";
 import { filterPhoneNumber } from "@/utils/phoneUtils";
 import { NextPage } from "next";
-import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import Calender from "../Svg/Calender";
 import Logo from "../Svg/Logo";
-import Rashmin from "../app/Rashmin.png";
 import Button from "./components/Button";
-import ScheduleList from "./components/ScheduleList";
 
 type Props = {};
 
@@ -19,11 +16,10 @@ const Landing: NextPage<Props> = ({ }) => {
 
   const [name, setName] = useState('');
   const [whatsAppNumber, setWhatsAppNumber] = useState('');
-  const [city, setCity] = useState('');
   const [hasDoneCourse, setHasDoneCourse] = useState('');
   const [nameError, setNameError] = useState('');
   const [whatsAppNumberError, setWhatsAppNumberError] = useState('');
-  const [cityError, setCityError] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
   const [hasDoneCourseError, setHasDoneCourseError] = useState('');
 
   useEffect(() => {
@@ -33,45 +29,26 @@ const Landing: NextPage<Props> = ({ }) => {
 
 
   const handleSubmit = async () => {
-    console.log({ name, whatsAppNumber, city, hasDoneCourse });
-    if (validate()) {
-      // send to make.com
+    console.log({ name, whatsAppNumber, hasDoneCourse });
+    const whatsappLink = 'https://chat.whatsapp.com/EATaRjRQoUkKfANjdZ5dH5';
+
+    if (validate() && !isLoading) {
       try {
+        setIsLoading(true);
         const phone = filterPhoneNumber(whatsAppNumber);
         console.log("🚀 ~ file: landing.tsx:41 ~ handleSubmit ~ phone:", phone)
-        let formData = new FormData();
-        formData.append("name", name);
-        formData.append("whatsapp", phone);
-        formData.append("city", city);
-        formData.append("hasDoneCourse", hasDoneCourse);
-        await fetch("https://hook.eu2.make.com/2egoy29ug7hkwf86tgi21px5sqliwkhg", {
+        await fetch("https://dailyyoga.careergpt.workers.dev/webhook/mumbai", {
           method: "POST",
-          body: formData,
+          body: JSON.stringify({
+            name,
+            phone,
+            hasDoneCourse,
+          }),
         });
       } catch (error) {
         console.log("🚀 ~ file: page.js:35 ~ formSubmitHandler ~ error:", error);
       }
-
-      // redirect to whatsapp
-      let whatsappLink;
-      if (city === 'pune' && hasDoneCourse === 'Yes') {
-        whatsappLink = "https://chat.whatsapp.com/E1Ok9ej52DzCKyVlbKcvgN"
-      }
-      else if (city === 'pune' && hasDoneCourse === 'No') {
-        whatsappLink = "https://chat.whatsapp.com/JZ1WjQEdcHMImnN5OkvIWZ"
-      }
-      else if (city === 'mumbai' && hasDoneCourse === 'Yes') {
-        whatsappLink = "https://chat.whatsapp.com/DR5xrn6zSOp6mCTY2NUvz8"
-      }
-      else if (city === 'mumbai' && hasDoneCourse === 'No') {
-        whatsappLink = "https://chat.whatsapp.com/HyiBYkxWaLs5diNuhcSSnD"
-      }
-      else if (city === 'other' && hasDoneCourse === 'Yes') {
-        whatsappLink = "https://chat.whatsapp.com/HoqBXsXanZn4D0psFMrt3y"
-      }
-      else if (city === 'other' && hasDoneCourse === 'No') {
-        whatsappLink = "https://chat.whatsapp.com/BWYgRiPfnwIDeWrZP1djQ4"
-      }
+      setIsLoading(false);
       //@ts-ignore
       window.fbq('track', 'Submit Form');
       router.push(`/thankyou?link=${whatsappLink}`)
@@ -88,10 +65,6 @@ const Landing: NextPage<Props> = ({ }) => {
       setWhatsAppNumberError("*required field");
       status = false;
     }
-    if (city === "") {
-      setCityError("*required field");
-      status = false;
-    }
     if (hasDoneCourse === "") {
       setHasDoneCourseError("*required field");
       status = false;
@@ -102,12 +75,12 @@ const Landing: NextPage<Props> = ({ }) => {
   return (
     <div className="w-full grid place-items-center">
       {/* top-design */}
-      <div className="bg-primary w-[390px] h-[756px]">
+      <div className="bg-primary w-[390px] h-[680px]">
         <div className="flex items-center justify-center flex-col mt-[54px] mb-[10px]">
           <div>
             <Logo />
           </div>
-          <div className="font-normal text-lg ">Residential</div>
+          <div className="font-normal text-lg ">Offline</div>
           <div className="text-secondary font-bold  text-center my-2">
             <span className="text-[46px] leading-[50px]">Happiness </span>{" "}
             <span className="text-[46px]">Program</span>
@@ -115,14 +88,14 @@ const Landing: NextPage<Props> = ({ }) => {
           <div className="border border-t-secondary border-b-secondary border-l-transparent border-r-transparent px-1 my-[16px]">
             at{" "}
             <span className="font-semibold ">
-              Art of Living Triveni Ashram, Pune
+              The Art of Living, Malad (West)
             </span>
           </div>
           <div className="flex items-center justify-center">
             <span>
               <Calender />
             </span>
-            <span>13 - 15 August 2023 </span>
+            <span>25 - 27 August 2023 </span>
           </div>
         </div>
         {/* form-start */}
@@ -172,7 +145,7 @@ const Landing: NextPage<Props> = ({ }) => {
             )}
           </div>
 
-          <div className="flex flex-col w-[330px]">
+          {/* <div className="flex flex-col w-[330px]">
             <label className="mb-1 opacity-80" htmlFor="city">
               City
             </label>
@@ -194,7 +167,7 @@ const Landing: NextPage<Props> = ({ }) => {
                 {cityError}
               </div>
             )}
-          </div>
+          </div> */}
 
           <div className="flex flex-col w-[330px]">
             <label className="mb-1 opacity-80" htmlFor="ArtOfLiving">
@@ -224,7 +197,7 @@ const Landing: NextPage<Props> = ({ }) => {
         </div>
       </div>
 
-      <div
+      {/* <div
         className="w-[390px] flex items-center justify-center my-12 
       "
       >
@@ -253,9 +226,9 @@ const Landing: NextPage<Props> = ({ }) => {
             ></div>
           </div>
         </div>
-      </div>
+      </div> */}
 
-      <div className="bg-primary w-[390px] h-[689px]">
+      <div className="bg-primary w-[390px] h-[600px]">
         <h2
           className="text-secondary text-3xl font-bold mt-[60px] mb-[16px] flex
            justify-center"
@@ -297,112 +270,18 @@ const Landing: NextPage<Props> = ({ }) => {
               </div>
             </div>
           </div>
-          <li>
+          {/* <li>
             Fulfilled Morning Yoga, Blissful Meditations & Musical Evenings
-          </li>
+          </li> */}
           <li>Youth Activities, Games & Interactive Group Processes</li>
           <li>Opportunity to network with happy minds</li>
-          <li>Multiple accommodation options available</li>
+          {/* <li>Multiple accommodation options available</li> */}
         </ul>
         <div className="w-full flex justify-center my-5">
           <Button onClick={handleSubmit} />
         </div>
       </div>
 
-      <div className="w-[390px] ">
-        <h2 className="text-secondary font-bold text-center text-3xl mt-[50px] mb-[16px]">
-          Schedule
-        </h2>
-        <div className="space-y-4">
-          <div className="flex justify-center">
-            <ScheduleList
-              date="13 August"
-              list={[
-                "Reporting by 4 pm",
-                "Session starts at 5 pm",
-                "Understanding body - breath - mind",
-                "Learning & Experiencing Sudarshan Kriya",
-                "Dinner at 8 pm",
-              ]}
-            />
-          </div>
-          <div className="flex justify-center">
-            <ScheduleList
-              date="14 August"
-              list={[
-                "Morning Yoga at 6:30 am",
-                "Breakfast at 8:30 am",
-                "Morning Session starts at 10 am",
-                "Formula of Happy & Stress Free Life",
-                "Lunch at 1 pm",
-                "Afternoon Session starts at 2:30 pm",
-                "Practical tips to reduce anxiety & frustration",
-                "Ashram tour at 5 pm - River front, Campus, Goshala & much more...",
-                "Dinner at 7:30 pm followed by Games & Musical Evening",
-              ]}
-            />
-          </div>
-          <div className="flex justify-center">
-            <ScheduleList
-              date="15 August"
-              list={[
-                "Morning Yoga at 6:30 am followed by Independance Day Celebration",
-                "Breakfast at 8:30 am",
-                "Morning Session starts at 10 am",
-                "Dive deeper into the self",
-                "Home going instructions",
-                "Lunch at 1 pm",
-              ]}
-            />
-          </div>
-        </div>
-      </div>
-
-      <div className="w-[390px]  bg-primary h-[740px]">
-        <div className="text-center  my-8">
-          <Button onClick={handleSubmit} />
-        </div>
-        <h2 className="text-secondary text-center text-4xl font-bold">
-          Photo Gallery
-        </h2>
-        <div className="w-full relative px-4 ">
-          <Image
-            className="object-cover object-center h-[250px] w-[250px]  absolute top-[200px] left-[0px] rounded-full z-[5]"
-            src={'/2.png'}
-            width={250}
-            height={250}
-            alt=""
-          />
-          <Image
-            className="object-cover h-[200px] z-[1] w-[200px] rounded-full absolute top-[50px]"
-            src={'/2.png'}
-            width={250}
-            height={250}
-            alt=""
-          />
-          <Image
-            className="object-cover h-[195px] z-[2] w-[195px] rounded-full absolute top-[80px] left-[135px]"
-            src={'/3.jpeg'}
-            width={250}
-            height={250}
-            alt=""
-          />
-          <Image
-            className="object-cover h-[190px] z-[3] w-[190px] rounded-full absolute top-[210px] left-[200px]"
-            src={'/4.jpeg'}
-            width={250}
-            height={250}
-            alt=""
-          />
-          <Image
-            className="object-cover h-[190px] z-[4] w-[190px] rounded-full absolute top-[360px] left-[160px]"
-            src={'/5.png'}
-            width={250}
-            height={250}
-            alt=""
-          />
-        </div>
-      </div>
     </div>
   );
 };
