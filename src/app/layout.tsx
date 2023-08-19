5dac99062c97b2476ade2b9095603efce770a66c
Rhutik <rhutikgiradkar@gmail.com>
2023-08-20 00:47:21 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: 'The Art Of Living',
   description: 'Happiness Program by The Art Of Living'
+}
 
 export default function RootLayout({ children }) {
   return (
